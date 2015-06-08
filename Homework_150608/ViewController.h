5b274b60cfabe0c1f596ace55238b3ccaecd5b80
peoplewhite <shonshon7@gmail.com>
2015-06-08 20:20:55 +0800

    first commit

---
+++ Homework_150608/ViewController.h
@@ -0,0 +1,21 @@
+//
+//  ViewController.h
+//  Homework_150608
+//
+//  Created by Po-Hao Cheng on 2015/6/8.
+//  Copyright (c) 2015年 phc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    IBOutlet UILabel *label1;
+    IBOutlet UILabel *label2;
+    IBOutlet UILabel *label3;
+    IBOutlet UILabel *label4;
+    IBOutlet UILabel *label5;
+}
+
+@end
+
---
+++ Homework_150608/ViewController.m
@@ -0,0 +1,32 @@
+//
+//  ViewController.m
+//  Homework_150608
+//
+//  Created by Po-Hao Cheng on 2015/6/8.
+//  Copyright (c) 2015年 phc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    label1.text = @"探索";
+    label2.text = @"搜尋";
+    label3.text = @"我的票券";
+    label4.text = @"現場互動";
+    label5.text = @"設定";
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
